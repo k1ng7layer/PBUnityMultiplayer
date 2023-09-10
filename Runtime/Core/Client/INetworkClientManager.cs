4c5d3438ce331fe631251b02e0ede47a1d6c0f95
k1ng7layer <75398051+k1ng7layer@users.noreply.github.com>
2023-09-10 21:04:30 +0300

    fix INetworkClientManager

--- Runtime/Core/Client/INetworkClientManager.cs
+++ Runtime/Core/Client/INetworkClientManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Net;
 using Cysharp.Threading.Tasks;
 using PBUdpTransport.Utils;
 using PBUnityMultiplayer.Runtime.Core.Authentication;
+using PBUnityMultiplayer.Runtime.Core.Spawn.SpawnHandlers;
 using UnityEngine;
 
 namespace PBUnityMultiplayer.Runtime.Core.Client
@@ -13,5 +15,7 @@ namespace PBUnityMultiplayer.Runtime.Core.Client
         void SendMessage<T>(T message, ESendMode sendMode);
         void Spawn(int prefabId, Vector3 position, Quaternion rotation);
         void Spawn<T>(int prefabId, Vector3 position, Quaternion rotation, T message);
+        void RegisterMessageHandler<T>(Action<T> handler) where T : struct;
+        void RegisterSpawnHandler<T>(NetworkSpawnHandler<T> handler) where T: struct;
     }
 }
